2f66e9237211b6072c2f155dfbafd3e506980a4d
ANUBHAV LAL <anubhavlal.15@gmail.com>
2022-08-20 00:11:21 +0530

    PWA update

--- src/index.js
+++ src/index.js
@@ -2,37 +2,38 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
+serviceWorkerRegistration.register();
 // Diable input from user
 (document.onkeydown = function (event) {
-   if (event.keyCode == 123) {
-     return false;
-   } else if (event.ctrlKey && event.shiftKey && event.keyCode == 73) {
-     return false;
-   } else if (event.ctrlKey && event.shiftKey && event.keyCode == 67) {
-     return false;
-   } else if (event.ctrlKey && event.shiftKey && event.keyCode == 86) {
-     return false;
-   } else if (event.ctrlKey && event.shiftKey && event.keyCode == 117) {
-     return false;
-   } else if (event.ctrlKey && event.keyCode == 85) {
-     return false;
-   }
- }),
-   false;
- if (document.addEventListener) {
-   document.addEventListener(
-     "contextmenu",
-     function (e) {
-       e.preventDefault();
-     },
-     false
-   );
- } else {
-   document.attachEvent("oncontextmenu", function () {
-     window.event.returnValue = false;
-   });
- }
- 
+  if (event.keyCode == 123) {
+    return false;
+  } else if (event.ctrlKey && event.shiftKey && event.keyCode == 73) {
+    return false;
+  } else if (event.ctrlKey && event.shiftKey && event.keyCode == 67) {
+    return false;
+  } else if (event.ctrlKey && event.shiftKey && event.keyCode == 86) {
+    return false;
+  } else if (event.ctrlKey && event.shiftKey && event.keyCode == 117) {
+    return false;
+  } else if (event.ctrlKey && event.keyCode == 85) {
+    return false;
+  }
+}),
+  false;
+if (document.addEventListener) {
+  document.addEventListener(
+    "contextmenu",
+    function (e) {
+      e.preventDefault();
+    },
+    false
+  );
+} else {
+  document.attachEvent("oncontextmenu", function () {
+    window.event.returnValue = false;
+  });
+}
